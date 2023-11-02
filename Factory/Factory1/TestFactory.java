0f8d7b33d9bdb41f58fd62a94cfb74d355da7dc2
Miza <mizael754@gmail.com>
2023-11-01 23:34:59 -0500

    Factory-1

---
+++ Factory/Factory1/TestFactory.java
@@ -0,0 +1,11 @@
+package GIT.Patrones.Factory1;
+import java.util.Arrays;
+public class TestFactory {
+    public static void main(String[] args) {
+    BurguerFactory burgerFactory1 = new BurguerFactory();
+    Burguer cheeseBurger = burgerFactory1.createCheeseBurger();
+    System.out.println(Arrays.toString(cheeseBurger.getIngredients()));
+    Burguer DeluxeCheeseBurger = burgerFactory1.createDeluxeCheeseBurger();
+    System.out.println(Arrays.toString(DeluxeCheeseBurger.getIngredients()));
+    }
+}
